65931105741a6f8968c4c37ca5d5a9cdd2cdd620
rithi-ka-v <rithikalai927@gmail.com>
2026-05-20 21:40:37 +0530

    add the changes

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:5000'
+    : 'https://fooddeliveryapp-5sak.onrender.com';
